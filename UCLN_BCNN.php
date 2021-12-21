201514cba87376cc314df251dd2bc30f0274ce02
luanvo1910 <luanvtgcd19841@fpt.edu.vn>
2021-12-21 23:04:25 +0700

    UCLN_BCNN

--- UCLN_BCNN.php
+++ UCLN_BCNN.php
@@ -1,12 +1,42 @@
+<form action="#" method="post">
+<h2>Tinhs UCLN và BCNN của 2 số tự nhiên</h2>
+<table>
+    <tr>
+        <td>Nhập số tự nhiên a</td>
+        <td><input type="number" name="a" /></td>
+    </tr>
+    <tr>
+        <td>Nhập số tự nhiên b</td>
+        <td><input type="number" name="b" /></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><input type="submit" value="Kết quả"></td>
+    </tr>
+</table>
+</form>
+
 <?php
-function UCLN($a, $b)
-{
+function USCLN($a, $b) {
     if ($b == 0) return $a;
-    return UCLN($b, $a % $b);
+    return USCLN($b, $a % $b);
 }
 
-function BCNN($a, $b)
-{
-    return ($a * $b) / UCLN($a, $b);
+function BSCNN($a, $b) {
+    return ($a * $b) / USCLN($a, $b);
 }
+
+$a = 0;
+$b = 0;
+if (isset ($_POST ['a']))
+{
+    $a = $_POST ['a'];
+}
+if (isset ($_POST ['b']))
+{
+    $b = $_POST ['b'];
+}
+echo ("Ước số chung lớn nhất: " . USCLN($GLOBALS ['a'], $GLOBALS ['b']));
+echo ("<br>");
+echo ("Bội số chung nhỏ nhất: " . BSCNN($GLOBALS ['a'], $GLOBALS ['b']));
 ?>
